ca264c52febb44c47bd1e1d95b3a8826bc97e7ea
Cory Wallace <Cpwalla026@gmail.com>
2020-01-02 16:59:37 -0500

    playing around with combat functions

--- DnD Combat companion/game.js
+++ DnD Combat companion/game.js
@@ -6,6 +6,25 @@
 
 
 //*** Pseudo Code ***//
+
+//
+// 1. Create the player. Use an object to store all of the varios stats taken from the "Human_fighter" PDF.
+// 2. Create the enemey in the same fashion as the player.
+// 3. setup the mechanisms for combat using functions. This will be broken down into several steps and functions
+//    A. To determine an ability modifier without consulting the table, subtract 10 from the ability score and then divide the total by 2 (round down). 
+// 4. Setup the logic for a win  or lose scenario
+// 5. Give the player the option to Replay. Later iterations may offer the opportunity to face newer enemies
+
+
+//*** Code ***//
+let abilities = {
+    'str': '',
+    'dex': '',
+    'con': '',
+    'int': '',
+    'wis': '',
+    'cha': ''
+};
 // 1. Create the player. Use an object to store all of the varios stats taken from the "Human_fighter" PDF.
 let player = {
     'name': 'Chordey',
@@ -14,8 +33,8 @@ let player = {
     'con': 15,
     'int': 9,
     'wis': 13,
-    'cha': 11
-
+    'cha': 11,
+    'AC': 18
 };
 // 2. Create the enemey in the same fashion as the player.
 let mob = {
@@ -25,8 +44,35 @@ let mob = {
     'con': 10,
     'int': 10,
     'wis': 9,
-    'cha': 8
+    'cha': 8,
+    'AC': 15
 };
 // 3. setup the mechanisms for combat using functions. This will be broken down into several steps and functions
+function abilityMod(playerAbility){
+    let attackChance = Math.floor((playerAbility - 10)/2);
+    console.log(attackChance);
+};
+
+abilityMod(player.str);
+
+function die20Roll(){
+    var die1 = Math.floor(Math.random()*20)+1;
+    console.log(die1);
+    return die1;
+};
+die20Roll();
+
+function hitCount(die1, attackChance, mob){
+    let hitChance = die1 + attackChance;
+
+    if(hitChance > mob.AC){
+        console.log("Hit!");
+        return "Hit!";
+    } else {
+        Console.log("Miss!");
+        "Miss!";
+    }
+};
+die20Roll();
 // 4. Setup the logic for a win  or lose scenario
 // 5. Give the player the option to Replay. Later iterations may offer the opportunity to face newer enemies
