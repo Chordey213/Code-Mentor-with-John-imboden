c6727b12bef8b26b6d122e1bf7d7ba60576dc563
Cory Wallace <Cpwalla026@gmail.com>
2020-01-03 15:48:54 -0500

    branching from work pc

--- Card games/War/game.js
+++ Card games/War/game.js
@@ -1,4 +1,4 @@
- //*** Pseudo Code ***//
+//*** Pseudo Code ***//
 
 
 // The game is War. The idea is to have a higher card than the NPC, in order to collect all cards in the stack. 
@@ -8,6 +8,50 @@
 //     A. Define a card Object we need a suite, a face value, a computational value
 //        (A mathematic value for the card, used for comparing one card to another),
 //        Asset Location(The location of the image of the card in the folder structure).
+let card = [
+    ["two", 2],
+    ["three", 3],
+    ["four", 4],
+    ["five", 5],
+    ["six", 6],
+    ["seven", 7],
+    ["eight", 8],
+    ["nine", 9],
+    ["ten", 10],
+    ["jack", 11],
+    ["queen", 12],
+    ["king", 13],
+    ["ace", 14]
+];
+
+let suites = [
+    "hearts", "diamonds", "spades", "clubs"
+];
+
+// This object builds one card from the cards and suites array literals
+let deck = {
+    newCard: {
+        suite: suites[0],
+        faceValue: card[0][0],
+        computationalValue: card[0][1],
+        assetLocation: "assets/" + card[0][1] + "_of_" + suites[0] + ".svg"
+    }
+};
+//you need to break down exactly how to build a deck, given the information that you have.
+// I need to place 14 cards in 4 suites
+function buildDeck(card, suites){
+    
+}
+
+function displayCard(deck) {
+    let cardImage = document.getElementById("img").src = deck.newCard.assetLocation;
+    return cardImage;
+};
+displayCard(deck);
+
+
+console.log(deck);
+
 // 2. Shuffle all of the cards
 //     B. develop a way to unstack the deck, and randomize the order. 
 // 3. divvy cards out to players
@@ -20,4 +64,4 @@
 //      D. The winner takes the cards from the round, and places them on the bottom of their deck.
 // 5. Repeat gameplay until one of the collections reaches a count of Zero.
 //      A. Once this happens, a winner is declared. Thbe winner will be placed on screen. 
-//      B. Capture the player's name before the game starts.
+//      B. Capture the player's name before the game starts
