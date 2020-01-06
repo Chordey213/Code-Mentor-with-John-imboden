bb8f3c568d6e7708fe3e6232211e9ad3f8b1ccec
Chordey213 <cpwalla026@gmail.com>
2020-01-05 22:33:02 -0500

    Shuffle deck works, can display card starting play

--- Card games/War/game.js
+++ Card games/War/game.js
@@ -8,7 +8,7 @@
 //     A. Define a card Object we need a suite, a face value, a computational value
 //        (A mathematic value for the card, used for comparing one card to another),
 //        Asset Location(The location of the image of the card in the folder structure).
-let playDeck = [];
+var newDeck = [];
 let card = [
     ["two", 2],
     ["three", 3],
@@ -29,53 +29,69 @@ let suites = [
     "hearts", "diamonds", "spades", "clubs"
 ];
 
-// This object builds one card from the cards and suites array literals
-let deck = {
-    newCard: {
-        suite: '',
-        faceValue: '',
-        computationalValue: '',
-        assetLocation: "assets/" + card[0][1] + "_of_" + suites[0] + ".svg"
-    }
-};
-//you need to break down exactly how to build a deck, given the information that you have.
-// I need to place 14 cards in 4 suites
-function buildDeck1(card){
-    for(i=0;i<card.length;i++){
-        for(j=0;j<card[i].length;j++){
-            deck.newCard.faceValue=card[i][j];
-            deck.newCard.computationalValue=card[i][j];
+function buildDeck1(card, suites) {
+    var playDeck = [];
+    for (i = 0; i < suites.length; i++) {
+        for (j = 0; j < card.length; j++) {
+            let newCard = {
+                suite: suites[i],
+                faceValue: card[j][0],
+                computationalValue: card[j][1],
+                assetLocation: "assets/" + card[j][1] + "_of_" + suites[i] + ".svg"
+            }
+            playDeck.push(newCard);
         }
-    } playDeck.push(deck);
+
+    }
+    return playDeck;
 };
-buildDeck1(card);
+newDeck = buildDeck1(card, suites);
 
-// function buildDeck2(suites){
-//     for(i=0;i<suites.length;i++){
-//         deck.newCard.suite=suites[i];
-//     }
-// };
-// buildDeck2(suites);
-
-function displayCard(deck) {
-    let cardImage = document.getElementById("img").src = deck.newCard.assetLocation;
-    return cardImage;
+function displaystack() {
+    let cardAImage = document.getElementById("playerdeck").src = "assets/Card_Back.svg";
+    let cardBImage = document.getElementById("npcdeck").src = "assets/Card_Back.svg";
+    return cardAImage, cardBImage;;
 };
-displayCard(deck);
-
-
-console.log(playDeck);
 
 // 2. Shuffle all of the cards
 //     B. develop a way to unstack the deck, and randomize the order. 
+function shuffleDeck(newDeck) {
+    var shuffledDeck = [];
+    for (i = 0; i < 52; i++) {
+        // shuffle the newDeck by removing cards at random and inserting them into the new deck
+        // create a variable to store one card, and remove one random card from newDeck
+        let cardIndex = Math.floor(Math.random() * newDeck.length) * 1;
+        //push that new card to the shuffled deck by index of the card
+        shuffledDeck.push(newDeck[cardIndex]);
+        // remove the card from newDeck, in order for it to be pushed to shuffledDeck, and not counted again during shuffling
+        newDeck.splice(cardIndex, 1);
+    }
+    return shuffledDeck;
+};
+newDeck = shuffleDeck(newDeck);
+
 // 3. divvy cards out to players
 //     A. Split the deck into two stacks. A collection for the User, and a Collection for the Computer
+var playerDeck = newDeck.splice(26);
+var npcDeck = newDeck;
+console.log("This is the players deck: ", playerDeck);
+console.log("This is the Computers deck: ", npcDeck);
 // 4. Begin gameplay
+function displayCard() {
+
+    return playerCardImage, npcCardImage;
+};
 //      A. the Player must click a button to draw a card from their stack
 //          Whenever the player draws the card, it is removed from the TOP of their deck, and from the collection
 //      B. The computer must draw a card from the top of its deck, and ensure that it is removed from the collection
 //      C. Evaluate which card has the higher computational value
 //      D. The winner takes the cards from the round, and places them on the bottom of their deck.
+function playGame(playerDeck) {
+    
+    displaystack();
+};
+playGame();
+
 // 5. Repeat gameplay until one of the collections reaches a count of Zero.
 //      A. Once this happens, a winner is declared. Thbe winner will be placed on screen. 
 //      B. Capture the player's name before the game starts
