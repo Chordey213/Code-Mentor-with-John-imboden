9e156ef308e14417fb0f7cf5946d1feb4af099ba
Cory Wallace <Cpwalla026@gmail.com>
2020-01-06 16:52:09 -0500

    fixed card return for player/npc

--- Card games/War/game.js
+++ Card games/War/game.js
@@ -31,6 +31,7 @@ let suites = [
 
 function buildDeck1(card, suites) {
     var playDeck = [];
+    
     for (i = 0; i < suites.length; i++) {
         for (j = 0; j < card.length; j++) {
             let newCard = {
@@ -39,13 +40,28 @@ function buildDeck1(card, suites) {
                 computationalValue: card[j][1],
                 assetLocation: "assets/" + card[j][1] + "_of_" + suites[i] + ".svg"
             }
+            // var cardImage = "assets/" + card[j][1] + "_of_" + suites[i] + ".svg"
+            // for(ext in cardImage){
+            //     for(i=0;i< cardImage[ext];i++){
+            //         src = "assets/" + cardImage + "_of_" + (i+1) + ".svg" + ext;
+            //         if(src != cardImage){
+            //             var cardImage = "assets/" + card[j][0] + "_of_" + suites[i] + ".svg" 
+            //             console.log(cardImage);
+            //             return cardImage 
+            //         }
+            //     }
+            // }
+
             playDeck.push(newCard);
+            // console.log(newCard.assetLocation);
         }
 
     }
     return playDeck;
 };
 newDeck = buildDeck1(card, suites);
+
+
 
 function displaystack() {
     let cardAImage = document.getElementById("playerdeck").src = "assets/Card_Back.svg";
@@ -77,20 +93,35 @@ var npcDeck = newDeck;
 console.log("This is the players deck: ", playerDeck);
 console.log("This is the Computers deck: ", npcDeck);
 // 4. Begin gameplay
-function displayCard() {
 
-    return playerCardImage, npcCardImage;
-};
+
 //      A. the Player must click a button to draw a card from their stack
 //          Whenever the player draws the card, it is removed from the TOP of their deck, and from the collection
 //      B. The computer must draw a card from the top of its deck, and ensure that it is removed from the collection
 //      C. Evaluate which card has the higher computational value
 //      D. The winner takes the cards from the round, and places them on the bottom of their deck.
-function playGame(playerDeck) {
-    
+function playGame() {
+    var playerCard = playerDeck.shift(1);   
+    var npcCard = npcDeck.shift(1);
+    displayCard(playerCard, npcCard);
+    console.log(playerCard);
+    console.log(npcCard);
     displaystack();
+    return playerCard, npcCard;
+
 };
-playGame();
+
+function displayCard(playerCard, npcCard) {
+    document.getElementById("player1Card").src=playerCard.assetLocation;
+    document.getElementById("npc1Card").src=npcCard.assetLocation;
+    return playerCard, npcCard;
+};
+
+function evaluate(playerCard, npcCard){
+    if(playerCard > npcCard){
+        
+    }
+}
 
 // 5. Repeat gameplay until one of the collections reaches a count of Zero.
 //      A. Once this happens, a winner is declared. Thbe winner will be placed on screen. 
