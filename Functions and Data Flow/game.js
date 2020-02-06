131ceff41da3512ff2b00e439ad45c4cba8e9030
Chordey213 <cpwalla026@gmail.com>
2020-02-05 19:34:39 -0500

    added click handler and finished storing clicks

--- Functions and Data Flow/game.js
+++ Functions and Data Flow/game.js
@@ -3,11 +3,36 @@ let comics = [
     "Spider-Man",
     "Venom",
     "Avengers",
-    "Hulk"
+    "Hulk",
+    "Ace the Bat-Hound",
+    "Allen, Barry",
+    "Allen, Bart",
+    "Allen, Crispus",
+    "Agent Liberty",
+    "Air Wave"
+
 ];
 
+var comicsClicked = [];
+
+function eventHandler(event){
+    var checked = event.checked;
+    if(checked === true){
+        comicsClicked.push(event.value);
+        document.getElementById('SearchedResults').innerHTML = comicsClicked;
+    } 
+
+    if(checked === false){
+        var clicked = comicsClicked.indexOf(event.value);
+        comicsClicked.splice(clicked, 1);
+        document.getElementById('SearchedResults').innerHTML = comicsClicked;
+    }
+    console.log(event);
+};
+
 function createCheckbox(comicTitle){
-    let strComicTitle = `<label><input type="checkbox"><span>` + comicTitle + `</span></label>`;
+    let strComicTitle = `<label><input type="checkbox" onClick='eventHandler(this)' value= '` + comicTitle + `'><span>` + comicTitle + `</span></label>`;
+
     return strComicTitle
 };
 
@@ -16,10 +41,10 @@ function printToScreen(stringsToPrint) {
         let htmlString = "";
         for (let i = 0; i < stringsToPrint.length; i++) {
             htmlString += createCheckbox(stringsToPrint[i]);
-            htmlString += "<br>";
-            htmlString += '<div id="SearchedResults"></div>';
+            htmlString += "<br>";            
         }
         htmlString += `<span id="numberofstringsprinted"></span>`
+        htmlString += '<div id="SearchedResults"></div>';
         document.getElementById("results").innerHTML += htmlString;
 
         return stringsToPrint.length;
@@ -28,8 +53,9 @@ function printToScreen(stringsToPrint) {
 
 //<!----- MVP Description -----!>//
 // Setup a Div with an Id for the purpose of being able to display the selected results
+// <div id="results"></div>
 // Create an OnClick handler (a function) that registers to the Onlcick event, passing in the checkboxes that are checked
-    // use an if statement, so that if the checkbox is selected, the item goes into the div. If the item is then unchecked, it removes from the div
+// use an if statement, so that if the checkbox is selected, the item goes into the div. If the item is then unchecked, it removes from the div
 // set the value of the checkbox to be the same value of what's going into the span (the comic title)
 // create a global collection of the selected comics, and update the onlclick handler to either add to, or take away from the list of comics collected
 // Update the contents of the "Selected Results" div with the collection of comics that were selected. 
