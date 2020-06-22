75a12d484cc559c1852c98c89326d81c1ef20c10
Chordey213 <cpwalla026@gmail.com>
2020-06-21 22:23:46 -0400

    added address add function

--- Address_Express/Routing/addressRouting.js
+++ Address_Express/Routing/addressRouting.js
@@ -1,5 +1,6 @@
 // handles just the /address route
 var {Address} = require('../Models/Models.js');
+const addressController = require('../controller/addressController');
 
 function routeRegister(router){
     router.get('/address', function (req, res) {
@@ -14,7 +15,10 @@ function routeRegister(router){
     });
 
     router.put('/address', async function (req, res){
-        
+        var addr = new Address(req.body.housenumber, req.body.streetname, req.body.city, req.body.state, req.body.zip, req.body.unit);
+        console.log(addr);  
+        await addressController.putAddress(addr);
+        res.json('Address Successfully added');
     });
 
     router.delete('/address', function (req, res){
